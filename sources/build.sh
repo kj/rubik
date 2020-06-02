1a1b8063c361cb18bacdfe1cce56eb6625da1773
Kelsey Judson <kelseyjudson@protonmail.com>
2020-06-01 22:00:08 -0500

    Fix tnum feature (but remove a glyph); temporary solution

---
+++ sources/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+fontmake -g Rubik.glyphs -i -o ttf --output-dir ../fonts/ttf/
+fontmake -g Rubik_Italic.glyphs -i -o ttf --output-dir ../fonts/ttf/
+fontmake -g Rubik.glyphs -i -o otf --output-dir ../fonts/otf/
+fontmake -g Rubik_Italic.glyphs -i -o otf --output-dir ../fonts/otf/
